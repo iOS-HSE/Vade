4df8723fcd3d453b91a7e1b36efcaed7942db7fb
LaCost765 <lacost765123@gmail.com>
2020-10-03 12:04:30 +0300

    Initial Commit

---
+++ Vade/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vade
+//
+//  Created by Egor on 03.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
