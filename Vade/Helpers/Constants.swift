4bf83a569df1aee0d77730ab48df9e446d537aa9
LaCost765 <lacost765123@gmail.com>
2020-10-04 20:39:45 +0300

    add auth with email and passwod

---
+++ Vade/Helpers/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  Vade
+//
+//  Created by Egor on 04.10.2020.
+//
+
+import Foundation
---
+++ Vade/Helpers/Utilities.swift
@@ -0,0 +1,52 @@
+//
+//  Utilities.swift
+//  Vade
+//
+//  Created by Egordash on 30/09/2020.
+//  Copyright © 2020 DevSquad. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Utilities
+{
+    static func styleTextField(_ textField: UITextField)
+    {
+        // create the bottom line
+        let bottomLine = CALayer()
+        
+        bottomLine.frame = CGRect(x: 0, y: textField.frame.height - 2, width: textField.frame.width, height: 2)
+        bottomLine.backgroundColor = UIColor.init(red: 48/255, green: 173/255, blue: 99/255, alpha: 1).cgColor
+        
+        // remove border on text field
+        textField.borderStyle = .none
+        
+        // add the line to the text field
+        textField.layer.addSublayer(bottomLine)
+    }
+    
+    static func styleFilledButton(_ button: UIButton)
+    {
+        // filled rounded corner style
+        button.backgroundColor = UIColor.init(red: 48/255, green: 173/255, blue: 99/255, alpha: 1)
+        button.layer.cornerRadius = 25.0
+        button.tintColor = UIColor.white
+    }
+    
+    static func styleHollowButton(_ button: UIButton)
+    {
+        // hollow rounded corner style
+        button.layer.borderWidth = 2
+        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.cornerRadius = 25.0
+        button.tintColor = UIColor.black
+    }
+    
+    static func isPasswordValid(_ password: String) -> Bool
+    {
+        let passText = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[a-z])(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&]{8,}")
+        
+        return passText.evaluate(with: password)
+    }
+}
