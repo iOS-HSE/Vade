26f58125cc911a30513d31d5e52b36f7937a86c3
Egor <lacost765123@gmail.com>
2020-11-18 01:12:05 +0300

    Изменения сториборда и файла проекта + сделал алерт как расширение

---
+++ Vade/Extensions/Extensions.swift
@@ -0,0 +1,30 @@
+//
+//  Extensions.swift
+//  Vade
+//
+//  Created by Egor on 17.11.2020.
+//
+
+import UIKit
+import Foundation
+
+extension UIViewController {
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
+             print("Ok button tapped")
+          })
+        alert.addAction(ok)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
